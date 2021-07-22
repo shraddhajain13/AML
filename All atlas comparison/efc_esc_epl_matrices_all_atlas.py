48342c21b1d3dbba7ea21ead9f99e40f77690fc1
Shraddha <s.jain@fz-juelich.de>
2021-07-23 00:29:30 +0200

    efc esc and epl matrix calculations

---
+++ All atlas comparison/efc_esc_epl_matrices_all_atlas.py
@@ -0,0 +1,57 @@
+import numpy as np
+import math
+import pandas as pd
+import scipy
+from scipy import stats
+import matplotlib.pyplot as plt
+import glob
+import os
+import ntpath
+
+path_list_for_fc = np.loadtxt("C:\\Users\\shrad\\OneDrive\\Desktop\\Juelich\\Internship\\Data\\path_fc.txt", dtype = str)
+path_list_for_sc = np.loadtxt("C:\\Users\\shrad\\OneDrive\\Desktop\\Juelich\\Internship\\Data\\path_sc.txt", dtype = str)
+sub_num_list_old = np.loadtxt("C:\\Users\\shrad\\OneDrive\\Desktop\\Juelich\\Internship\\Data\\List_23_28_54_49_118.txt", usecols=(0))
+
+r"""
+def efc_matrix():
+    for path in path_list_for_fc: #has the path for the concatenated files obtained after zscoring
+        atlas = str(ntpath.basename(path))
+        print(atlas) 
+        efc_matrix = []
+        for sub in sub_num_list_old:
+            sub = str((int)(sub))
+            print(sub)
+            #for filename in glob.glob(os.path.join(path, '*'+ sub + '*.csv'):
+            file_name = os.path.join(path, 'conc_'+ sub + '.csv')
+            data_fc = pd.read_csv(file_name, header = None).values
+            matrix_fc = np.zeros([data_fc.shape[1], data_fc.shape[1]])
+            #print(data_fc.shape[1])
+            for i in range(data_fc.shape[1]):
+                for j in range(data_fc.shape[1]):
+                    matrix_fc[i][j] = stats.pearsonr(data_fc[:, i], data_fc[:, j])[0]
+
+            upp_tri = matrix_fc[np.triu_indices(data_fc.shape[1], 1)]
+            #print(len(upp_tri))
+            efc_matrix.append(upp_tri)
+        np.savetxt(r"C:\Users\shrad\OneDrive\Desktop\Juelich\Internship\Data\FC_matrices_all_atlas\efc_" + atlas + ".csv", np.array(efc_matrix).transpose(), delimiter = ',')
+    
+"""
+#def sc_matrix():
+for path in path_list_for_sc:
+    atlas = str(ntpath.basename(path))
+    print(atlas)
+    esc_matrix = []
+    epl_matrix = []
+    for sub in sub_num_list_old:
+        sub = str((int)(sub))
+        print(sub)
+        file_sc = glob.glob(os.path.join(path, sub + '*17Networks_order_FSLMNI152_1mm_count.csv'))[0]
+        file_pl = glob.glob(os.path.join(path, sub + '*17Networks_order_FSLMNI152_1mm_length.csv'))[0]
+        data_sc = pd.read_csv(file_sc, header = None, delimiter = ' ')
+        data_pl = pd.read_csv(file_pl, header = None, delimiter = ' ')
+        matrix_sc = data_sc.values
+        matrix_pl = data_pl.values
+        esc_matrix.append(matrix_sc[np.triu_indices(data_sc.shape[1], 1)])
+        epl_matrix.append(matrix_pl[np.triu_indices(data_pl.shape[1], 1)])
+    np.savetxt(r"C:\Users\shrad\OneDrive\Desktop\Juelich\Internship\Data\SC_PL_matrices_all_atlas\esc_" + atlas + ".csv", np.array(esc_matrix).transpose(), delimiter = ',')   
+    np.savetxt(r"C:\Users\shrad\OneDrive\Desktop\Juelich\Internship\Data\SC_PL_matrices_all_atlas\epl_" + atlas + ".csv", np.array(epl_matrix).transpose(), delimiter = ',')
--- All atlas comparison/try.py
+++ All atlas comparison/try.py
@@ -7,7 +7,8 @@ import matplotlib.pyplot as plt
 import glob
 import os
 import ntpath
-import pingouin as pg
+
+r"""
 
 path = r"C:\Users\shrad\OneDrive\Desktop\Juelich\Internship\Data\HCP_Deriv\BOLD_time_17N_all\BOLD_time_17N_all"
 sub_num_list_old = np.loadtxt("C:\\Users\\shrad\\OneDrive\\Desktop\\Juelich\\Internship\\Data\\List_23_28_54_49_118.txt", usecols=(0))
@@ -22,3 +23,31 @@ for sub in sub_num_list_old:
         np.savetxt(r"C:\Users\shrad\OneDrive\Desktop\Juelich\Internship\Data\temp_zscore_S100\zscore_" + str(sub) + '_' + str(k) + '.csv', zscored_data, delimiter=",") 
         k = k + 1
 
+y = []
+for i in range(20):
+    x = np.linspace(1, 10, 10).tolist()
+    y.append(x)
+
+np.savetxt(r"C:\Users\shrad\OneDrive\Desktop\Juelich\Internship\Data\tryy.csv", np.array(y).transpose(), delimiter = ',')
+#z = np.array([[1,2,3], [4,5,6]]).transpose()
+#print(type(np.c_[z]))
+#print(z)
+"""
+sub_num_list_old = np.loadtxt("C:\\Users\\shrad\\OneDrive\\Desktop\\Juelich\\Internship\\Data\\List_23_28_54_49_118.txt", usecols=(0))
+path = "C:\\Users\\shrad\\OneDrive\\Desktop\\Juelich\\Internship\\Data\\Concatenated_all_atlas\\HO25"
+efc_matrix = []
+for sub in sub_num_list_old:
+    sub = str((int)(sub))
+    print(sub)
+    file_name = os.path.join(path, 'conc_'+ sub + '.csv')
+    data_fc = pd.read_csv(file_name, header = None).values
+    matrix_fc = np.zeros([data_fc.shape[1], data_fc.shape[1]])
+    #print(data_fc.shape[1])
+    for i in range(data_fc.shape[1]):
+        for j in range(data_fc.shape[1]):
+            matrix_fc[i][j] = stats.pearsonr(data_fc[:, i], data_fc[:, j])[0]
+
+    upp_tri = matrix_fc[np.triu_indices(data_fc.shape[1], 1)]
+    #print(len(upp_tri))
+    efc_matrix.append(upp_tri)
+np.savetxt(r"C:\Users\shrad\OneDrive\Desktop\Juelich\Internship\Data\efc_HO25_temp.csv", np.array(efc_matrix).transpose(), delimiter = ',')
